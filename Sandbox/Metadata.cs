9377dd23bbeacec189a74cbfc1d13c1d1e1f2b22
PieterVdB <Pieter.VdB@live.be>
2020-12-17 20:16:24 +0100

    Initial commit

---
+++ Sandbox/Metadata.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Sandbox
+{
+    [XmlRoot("Metadata")]
+    public class Metadata
+    {
+        [XmlElement("Attribute")]
+        public List<Attribute> Attributes { get; set; }
+    }
+
+    public class Attribute
+    {
+        [XmlElement("Schema")]
+        public string Schema { get; set; }
+
+        [XmlElement("Table")]
+        public string Table { get; set; }
+
+        [XmlElement("Column")]
+        public string Column { get; set; }
+
+        [XmlElement("Function")]
+        public string Function { get; set; }
+
+        [XmlElement("SourceColumn")]
+        public string SourceColumn { get; set; }
+
+        [XmlElement("Ordinal_Position")]
+        public string Ordinal_Position { get; set; }
+
+        [XmlElement("IsNullable")]
+        public bool IsNullable { get; set; }
+
+        [XmlElement("IsBusinessKey")]
+        public bool IsBusinessKey { get; set; }
+
+        [XmlElement("DataType")]
+        public string DataType { get; set; }
+
+        [XmlElement("Character_maximum_length")]
+        public int? Character_maximum_length { get; set; }
+
+        [XmlElement("Numeric_Precision")]
+        public int? Numeric_Precision { get; set; }
+
+        [XmlElement("Numeric_Scale")]
+        public int? Numeric_Scale { get; set; }
+
+        [XmlElement("DateTime_Precision")]
+        public int? DateTime_Precision { get; set; }
+    }
+}
---
+++ Sandbox/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Sandbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(Metadata));
+
+            using (FileStream fileStream = new FileStream(@"C:\Source\SimpleBI\DB\Metadata\UCI.xml", FileMode.Open))
+            {
+                Metadata result = (Metadata)serializer.Deserialize(fileStream);
+            }
+        }
+    }
+}
