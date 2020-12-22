fa2911e77afa331e4588a8ebfb9ff6a9fc65ac39
PieterVdB <Pieter.VdB@live.be>
2020-12-22 22:13:39 +0100

    Started adding viewlayer

---
+++ DB/LZ_UCI/Views/Riders.sql
@@ -0,0 +1,17 @@
+﻿CREATE VIEW LZ_UCI.vw_Riders
+AS
+SELECT
+[Function] AS [Function],
+[Last Name] AS [Last Name],
+[First Name] AS [First Name],
+[Birth date] AS [Birth date],
+[Birth date year] AS [Birth date year],
+[Birth Year] AS [Birth Year],
+[Gender] AS [Gender],
+[Category] AS [Category],
+[Country] AS [Country],
+[Continent] AS [Continent],
+[Team Code] AS [Team Code],
+[Team Name] AS [Team Name],
+[UCIID] AS [UCIID]
+FROM LZ_UCI.Riders
---
+++ DB/LZ_UCI/Views/Teams.sql
@@ -0,0 +1,12 @@
+﻿CREATE VIEW LZ_UCI.vw_Teams
+AS
+SELECT
+[Code] AS [Code],
+[Name] AS [Name],
+[Team Category] AS [Team Category],
+[Country] AS [Country],
+[Continent] AS [Continent],
+[Format] AS [Format],
+[Email] AS [Email],
+[Website] AS [Website]
+FROM LZ_UCI.Teams
